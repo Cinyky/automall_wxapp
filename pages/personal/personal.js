cb924fa3d4594de4c24725f2a467c1303da3b565
Cyy <cyy1079276272@163.com>
2019-07-01 11:57:35 +0800

    个人中心&商家

---
+++ pages/personal/personal.js
@@ -0,0 +1,15 @@
+// pages/personal/personal.js
+const util = require('../../utils/util.js')
+
+Page({
+    data: {
+      
+    },
+    onLoad: function () {
+
+    },
+    onReady: function (e) {
+
+    },
+    
+})
---
+++ pages/shop/shop.js
@@ -0,0 +1,47 @@
+// pages/shop/shop.js
+
+
+Page({
+    data: {
+
+        latitude: 23.099994,
+        longitude: 113.324520,
+        markers: [{
+            id: 1,
+            latitude: 23.099994,
+            longitude: 113.324520,
+            name: 'T.I.T 创意园',
+            iconPath: '/assets/image/location.png'
+        }],
+        covers: [{
+            latitude: 23.099994,
+            longitude: 113.344520,
+            iconPath: '/assets/image/location.png'
+        }, {
+            latitude: 23.099994,
+            longitude: 113.304520,
+            iconPath: '/assets/image/location.png'
+        }],
+    },
+    onLoad: function () {
+      
+    },
+    onReady: function (e) {
+        this.mapCtx = wx.createMapContext('myMap')
+    },
+    map: function () {
+        let self = this;
+        wx.getLocation({
+            type: 'gcj02', //返回可以用于wx.openLocation的经纬度
+            success: function (res) {
+                var latitude = res.latitude
+                var longitude = res.longitude
+                wx.openLocation({
+                    latitude: self.data.latitude,
+                    longitude: self.data.longitude,
+                    scale: 28
+                })
+            }
+        });
+    },
+})
