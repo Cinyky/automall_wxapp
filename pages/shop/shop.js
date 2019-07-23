ecc0f06c69de2d2c944b1945eb358278a570046c
Cyy <cyy1079276272@163.com>
2019-07-23 14:51:34 +0800

    公告展示

--- pages/shop/shop.js
+++ pages/shop/shop.js
@@ -1,5 +1,6 @@
 // pages/shop/shop.js
 
+
 const shopApi = require('../../api/shopApi.js');
 const fileUtil = require('../../utils/fileUtil.js')
 Page({
@@ -9,6 +10,12 @@ Page({
         shop: null,
         imagewidth: 0,
         imageheight : 0,
+        marqueePace: 1,//滚动速度
+        marqueeDistance: 0,//初始滚动距离
+        marquee_margin: 30,
+        size:14,
+        interval: 20 // 时间间隔
+
     },
     onLoad: function () {
         let self = this;
@@ -29,7 +36,7 @@ Page({
             self.setData({
                 shop: shop,
                 markers: markers
-            })
+            }),self.scroll();
         };
         apiObj.fail = function (res) {
             console.log("fail", res);
@@ -61,7 +68,51 @@ Page({
           imagewidth: imageSize.imageWidth,
           imageheight: imageSize.imageHeight
         })
+    },
+
+    onShow: function () {
+       
       },
+
+    scroll: function() {
+        var that = this;
+        var length = that.data.shop.description.length * that.data.size;//文字长度
+        var windowWidth = wx.getSystemInfoSync().windowWidth;// 屏幕宽度
+        //console.log(length,windowWidth);
+        that.setData({
+          length: length,
+          windowWidth: windowWidth
+        });
+        that.scrolltxt();// 第一个字消失后立即从右边出现
+    },
+      
+    scrolltxt: function () {
+        var that = this;
+        var length = that.data.length;//滚动文字的宽度
+        var windowWidth = that.data.windowWidth;//屏幕宽度
+        if (length < windowWidth){
+          that.setData({ marquee_margin:"1000"});//只显示一条不滚动右边间距加大，防止重复显示
+        } 
+        var interval = setInterval(function () {
+        var maxscrollwidth = length + that.data.marquee_margin;//滚动的最大宽度，文字宽度+间距，如果需要一行文字滚完后再显示第二行可以修改marquee_margin值等于windowWidth即可
+        var crentleft = that.data.marqueeDistance;
+        if (crentleft < maxscrollwidth) {//判断是否滚动到最大宽度
+            that.setData({
+            marqueeDistance: crentleft + that.data.marqueePace
+            })
+        }
+        else {
+            //console.log("替换");
+            that.setData({
+            marqueeDistance: 0 // 直接重新滚动
+            });
+            clearInterval(interval);
+            that.scrolltxt();
+        }
+        }, that.data.interval);
+        
+    }
+    
       
     
 })
